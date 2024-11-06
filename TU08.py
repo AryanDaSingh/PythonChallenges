0a3e222b0c30dc4286eeabea72d4053cfa50113a
Aryan Singh <113500849+AryanDaSingh@users.noreply.github.com>
2024-11-06 09:01:44 +0800

    Add files via upload

---
+++ TU08.py
@@ -0,0 +1,9 @@
+a = int(input("which excersize: "))
+if a == 1:
+    sub = "Computer Science"
+    while True:
+        wy = input("What is the best subject")
+        if wy == sub:
+            print("good")
+        else:
+            print("Try again")
