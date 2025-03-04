78adc90851e0818b1130a741dc8e3016b899dd7f
Aryan Singh <113500849+AryanDaSingh@users.noreply.github.com>
2025-03-04 08:50:41 +0800

    Update TU07.py

--- TU07.py
+++ TU07.py
@@ -1,8 +1,11 @@
-a = int(input("which excersize: "))
-if a == 1:
+exc = int(input("Which challenge:   "))
+if exc == 1:
     for i in range(1,101):
         print(i)
-elif a == 2:
-    for i in range(1,101):
-        if i%2==0:
+elif exc == 2:
+    for i in range(0,101):
+        if i % 2 == 0:
             print(i)
+elif exc == 3:
+    for i in range(1000,-1,-10):
+        print(i)
